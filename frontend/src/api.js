ca932213a68a3e7cd0229ea1914d08343982bb6c
Chandu <dulamchandu13@gmail.com>
2025-08-27 02:49:21 +0530

    Minor fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:5000/api";
-const API_URL = "https://repofinder-backend.onrender.com";
+const API_URL = "https://repofinder-backend.onrender.com/api";
 
 export const searchRepos = (keyword, page = 1, perPage = 10) =>
   axios.post(`${API_URL}/search`, { keyword, page, perPage });
